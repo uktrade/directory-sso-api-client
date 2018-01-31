7ba5f45504c1228788d631a7141fef7a0cca5499
kowalcj0 <kowalczykjanusz@gmail.com>
2018-01-31 16:43:40 +0000

    support edge cases, add arg types & better formatting

--- directory_sso_api_client/testapiclient.py
+++ directory_sso_api_client/testapiclient.py
@@ -4,13 +4,16 @@ from directory_sso_api_client.base import BaseAPIClient
 class DirectoryTestAPIClient(BaseAPIClient):
 
     endpoints = {
-        'user_by_email': 'testapi/user-by-email/%s/'
+        'user_by_email': 'testapi/user-by-email/{email}/'
     }
 
     def __init__(self, base_url=None, api_key=None):
         super(DirectoryTestAPIClient, self).__init__(base_url, api_key)
 
-    def get_user_by_email(self, email, token):
-        url = self.endpoints['user_by_email'] % email
-        params = {"token": token}
-        return self.get(url=url, params=params)
+    def get_user_by_email(self, email: str, token: str):
+        result = None
+        if email and token:
+            url = self.endpoints['user_by_email'].format(email=email)
+            params = {"token": token}
+            result = self.get(url=url, params=params)
+        return result
