c36a41d891658e61d1d82bece7b50f6f8e4903f6
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-06 10:58:47 +0000

    use HAWK signature check rather than token verification

--- directory_sso_api_client/testapiclient.py
+++ directory_sso_api_client/testapiclient.py
@@ -7,13 +7,9 @@ class DirectoryTestAPIClient(BaseAPIClient):
         'user_by_email': 'testapi/user-by-email/{email}/'
     }
 
-    def __init__(self, base_url=None, api_key=None):
-        super(DirectoryTestAPIClient, self).__init__(base_url, api_key)
-
-    def get_user_by_email(self, email: str, token: str):
+    def get_user_by_email(self, email: str):
         result = None
-        if email and token:
+        if email:
             url = self.endpoints['user_by_email'].format(email=email)
-            params = {"token": token}
-            result = self.get(url=url, params=params)
+            result = self.get(url=url)
         return result
