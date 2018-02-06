4df77dce3608ba9e4015ffadebf529fb9b05f07a
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-06 11:10:05 +0000

    update unit tests to match current state

--- tests/test_testapiclient.py
+++ tests/test_testapiclient.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from directory_sso_api_client.testapiclient import DirectoryTestAPIClient
 
@@ -7,7 +7,7 @@ from tests import stub_request
 
 class DirectoryTestAPIClientTest(TestCase):
 
-    endpoint = 'https://example.com/testapi/user-by-email/'
+    url = 'https://example.com/testapi/user-by-email/'
 
     def setUp(self):
         self.base_url = 'https://example.com'
@@ -19,40 +19,38 @@ class DirectoryTestAPIClientTest(TestCase):
         for endpoint in self.client.endpoints.values():
             assert not endpoint.startswith('/')
 
-    @stub_request(endpoint + 'test@example.com/', 'get')
+    @stub_request(url + 'test@example.com/', 'get')
     def test_get_user_by_email(self, stub):
         email = "test@example.com"
-        response = self.client.get_user_by_email(email=email, token="debug")
+        response = self.client.get_user_by_email(email=email)
         request = stub.request_history[0]
         assert request.url == response.url
 
-    @stub_request(endpoint + 'test@example.com/', 'get')
-    def test_client_should_pass_token(self, stub):
-        email = "test@example.com"
-        response = self.client.get_user_by_email(email=email, token="debug")
-        assert "?token=debug" in response.url
-
-    @stub_request(endpoint + 'test@example.com/', 'get', 404)
-    def test_should_return_404_on_wrong_token(self, stub):
-        email = "test@example.com"
-        response = self.client.get_user_by_email(email=email, token="invalid")
-        assert response.status_code == 404
-
-    @stub_request(endpoint + 'test@example.com/', 'get', 404)
-    def test_should_return_404_on_missing_token(self, stub):
-        email = "test@example.com"
-        response = self.client.get_user_by_email(email=email, token=None)
-        assert response is None
-
     def test_should_return_none_on_missing_email(self):
-        response = self.client.get_user_by_email(email="", token=None)
+        response = self.client.get_user_by_email(email=None)
         assert response is None
 
     def test_should_return_none_on_empty_email(self):
-        response = self.client.get_user_by_email(email=None, token=None)
+        response = self.client.get_user_by_email(email="")
         assert response is None
 
-    def test_should_not_pass_empty_token_query_parameter(self):
-        email = "test@example.com"
-        response = self.client.get_user_by_email(email=email, token=None)
-        assert response is None
+    @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
+    def test_should_not_make_any_request_on_empty_email(self, mocked_request):
+        self.client.get_user_by_email(email="")
+        assert mocked_request.call_count == 0
+
+    @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
+    def test_should_not_make_any_request_on_no_email(self, mocked_request):
+        self.client.get_user_by_email(email=None)
+        assert mocked_request.call_count == 0
+
+    @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
+    def test_get_last_login_without_params(self, mocked_request):
+        email = "test@user.com"
+        self.client.get_user_by_email(email=email)
+
+        assert mocked_request.call_count == 1
+        assert mocked_request.call_args == mock.call(
+            method='GET', params=None,
+            url='testapi/user-by-email/{}/'.format(email), headers=None,
+        )
