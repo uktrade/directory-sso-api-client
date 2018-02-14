f0922267fa5d4e7ce6fd27e08d67abd55685c7e1
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-14 06:29:31 +0000

    add option to flag user email as verified or not

--- directory_sso_api_client/testapiclient.py
+++ directory_sso_api_client/testapiclient.py
@@ -14,3 +14,8 @@ class DirectoryTestAPIClient(BaseAPIClient):
     def delete_user_by_email(self, email):
         url = self.endpoints['user_by_email'].format(email=email)
         return self.delete(url=url)
+
+    def flag_user_email_as(self, email, verified):
+        url = self.endpoints['user_by_email'].format(email=email)
+        data = {"is_verified": verified}
+        return self.patch(url=url, data=data)
