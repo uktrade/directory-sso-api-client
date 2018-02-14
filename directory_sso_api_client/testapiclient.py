15a7b3ac9ad837ed62807d90071e44458917f29a
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-14 14:21:33 +0000

    give more descriptive name

--- directory_sso_api_client/testapiclient.py
+++ directory_sso_api_client/testapiclient.py
@@ -15,7 +15,7 @@ class DirectoryTestAPIClient(BaseAPIClient):
         url = self.endpoints['user_by_email'].format(email=email)
         return self.delete(url=url)
 
-    def flag_user_email_as(self, email, verified):
+    def flag_user_email_as_verified_or_not(self, email, verified):
         url = self.endpoints['user_by_email'].format(email=email)
         data = {"is_verified": verified}
         return self.patch(url=url, data=data)
--- tests/test_testapiclient.py
+++ tests/test_testapiclient.py
@@ -100,36 +100,36 @@ class DirectoryTestAPIClientTest(TestCase):
     @stub_request(url + 'test@example.com/', 'patch')
     def test_flag_user_email_as_verified_or_not(self, stub):
         email = 'test@example.com'
-        response = self.client.flag_user_email_as(email, True)
+        response = self.client.flag_user_email_as_verified_or_not(email, True)
         request = stub.request_history[0]
         assert request.url == response.url
 
     @stub_request(url + 'None/', 'patch', 404)
     def test_flag_user_should_return_404_on_no_email(self, stub):
-        response = self.client.flag_user_email_as(email=None, verified=True)
+        response = self.client.flag_user_email_as_verified_or_not(email=None, verified=True)
         assert response.status_code == 404
 
     @stub_request(url, 'patch', 404)
     def test_flag_user_should_return_404_on_empty_email(self, stub):
-        response = self.client.flag_user_email_as(email='', verified=False)
+        response = self.client.flag_user_email_as_verified_or_not(email='', verified=False)
         assert response.status_code == 404
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_flag_user_should_make_request_on_empty_email(
             self, mocked_request):
-        self.client.flag_user_email_as(email='', verified=True)
+        self.client.flag_user_email_as_verified_or_not(email='', verified=True)
         assert mocked_request.call_count == 1
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_flag_user_should_make_request_on_no_email(
             self, mocked_request):
-        self.client.flag_user_email_as(email=None, verified=True)
+        self.client.flag_user_email_as_verified_or_not(email=None, verified=True)
         assert mocked_request.call_count == 1
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_flag_user_check_request_arguments(self, mocked_request):
         email = 'test@user.com'
-        self.client.flag_user_email_as(email=email, verified=True)
+        self.client.flag_user_email_as_verified_or_not(email=email, verified=True)
         assert mocked_request.call_count == 1
         assert mocked_request.call_args == mock.call(
             method='PATCH',
