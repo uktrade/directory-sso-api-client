a1d5b67da9e00b268aa7c90b24ce1109435514b1
kowalcj0 <kowalczykjanusz@gmail.com>
2018-02-14 06:30:12 +0000

    Add missing stubs & use `'` consistently

--- tests/test_testapiclient.py
+++ tests/test_testapiclient.py
@@ -21,22 +21,24 @@ class DirectoryTestAPIClientTest(TestCase):
 
     @stub_request(url + 'test@example.com/', 'get')
     def test_get_user_by_email(self, stub):
-        email = "test@example.com"
+        email = 'test@example.com'
         response = self.client.get_user_by_email(email=email)
         request = stub.request_history[0]
         assert request.url == response.url
 
-    def test_get_user_should_return_404_on_missing_email(self):
+    @stub_request(url + 'None/', 'get', 404)
+    def test_get_user_should_return_404_on_missing_email(self, stub):
         response = self.client.get_user_by_email(email=None)
         assert response.status_code == 404
 
-    def test_get_user_should_return_404_on_empty_email(self):
-        response = self.client.get_user_by_email(email="")
+    @stub_request(url, 'get', 404)
+    def test_get_user_should_return_404_on_empty_email(self, stub):
+        response = self.client.get_user_by_email(email='')
         assert response.status_code == 404
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_get_user_should_make_request_on_empty_email(self, mocked_request):
-        self.client.get_user_by_email(email="")
+        self.client.get_user_by_email(email='')
         assert mocked_request.call_count == 1
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
@@ -46,7 +48,7 @@ class DirectoryTestAPIClientTest(TestCase):
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_get_user_by_mail_check_request_parameters(self, mocked_request):
-        email = "test@user.com"
+        email = 'test@user.com'
         self.client.get_user_by_email(email=email)
 
         assert mocked_request.call_count == 1
@@ -57,23 +59,25 @@ class DirectoryTestAPIClientTest(TestCase):
 
     @stub_request(url + 'test@example.com/', 'delete')
     def test_delete_user_by_email(self, stub):
-        email = "test@example.com"
+        email = 'test@example.com'
         response = self.client.delete_user_by_email(email=email)
         request = stub.request_history[0]
         assert request.url == response.url
 
-    def test_delete_user_should_return_404_on_missing_email(self):
+    @stub_request(url + 'None/', 'delete', 404)
+    def test_delete_user_should_return_404_on_missing_email(self, stub):
         response = self.client.delete_user_by_email(email=None)
         assert response.status_code == 404
 
-    def test_delete_user_should_return_404_on_empty_email(self):
-        response = self.client.delete_user_by_email(email="")
+    @stub_request(url, 'delete', 404)
+    def test_delete_user_should_return_404_on_empty_email(self, stub):
+        response = self.client.delete_user_by_email(email='')
         assert response.status_code == 404
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_delete_user_should_make_request_on_empty_email(
             self, mocked_request):
-        self.client.delete_user_by_email(email="")
+        self.client.delete_user_by_email(email='')
         assert mocked_request.call_count == 1
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
@@ -84,7 +88,7 @@ class DirectoryTestAPIClientTest(TestCase):
 
     @mock.patch('directory_sso_api_client.base.BaseAPIClient.request')
     def test_delete_user_check_request_arguments(self, mocked_request):
-        email = "test@user.com"
+        email = 'test@user.com'
         self.client.delete_user_by_email(email=email)
 
         assert mocked_request.call_count == 1
