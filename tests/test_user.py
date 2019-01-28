d38a81075d5f7e2a40634970af6068bf25c35fe6
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-01-28 13:30:29 +0000

    sso-client for creating user profile

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,7 @@
 from unittest import TestCase, mock
 
 from tests import stub_request
-
+import json
 from directory_sso_api_client.user import UserAPIClient
 
 
@@ -120,4 +120,31 @@ class UserAPIClientTest(TestCase):
             method='POST',
             url='api/v1/user/',
             authenticator=None,
+        )
+
+    @mock.patch('directory_client_core.authentication.SessionSSOAuthenticator')
+    @mock.patch('directory_client_core.base.AbstractAPIClient.request')
+    def test_create_user_profile(self, mocked_request, mocked_authenticator):
+
+        user_profile_data = {
+            "first_name": "john",
+            "last_name": "smith",
+            "job_title": "director",
+            "mobile_phone_number": "0788712738738",
+        }
+
+        self.client.create_user_profile(
+            sso_session_id=123,
+            first_name=user_profile_data['first_name'],
+            last_name=user_profile_data['last_name'],
+            job_title=user_profile_data['job_title'],
+            mobile_phone_number=user_profile_data['mobile_phone_number']
+        )
+        assert mocked_request.call_count == 1
+        assert mocked_request.call_args == mock.call(
+            content_type='application/json',
+            data=json.dumps(user_profile_data),
+            method='POST',
+            url='api/v1/user/create-profile',
+            authenticator=mocked_authenticator(),
         )
