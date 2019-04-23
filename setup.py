27c8629506f6e60b75592ba6a0bec8986934e93d
richtier <rikatee@gmail.com>
2019-04-23 14:30:29 +0100

    Reduce noise

--- setup.py
+++ setup.py
@@ -24,10 +24,25 @@ setup(
     description='Python API client for Export Directory.',
     packages=find_packages(exclude=["tests.*", "tests"]),
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=[
-        'directory_client_core>=4.0.2,<5.0.0',
+        'directory_client_core>=5.0.0,<6.0.0',
     ],
+    extras_require={
+        'test': [
+            'django>=1.11.20,<2.0a1',
+            'requests>=2.18.4,<3.0.0',
+            'pytest==3.0.2',
+            'pytest-cov==2.3.1',
+            'flake8==3.0.4',
+            'requests_mock==1.1.0',
+            'codecov==2.0.9',
+            'twine>=1.11.0,<2.0.0',
+            'wheel>=0.31.0,<1.0.0',
+            'setuptools>=38.6.0,<39.0.0',
+        ]
+    },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -37,7 +52,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
