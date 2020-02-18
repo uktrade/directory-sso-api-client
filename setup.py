b28c2e1540b12908fbb9763662c8cff3c4c13d1b
kowalcj0 <kowalczykjanusz@gmail.com>
2020-02-18 14:54:53 +0000

    update dependencies to the latest available versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='directory_sso_api_client',
-    version='6.2.0',
+    version='6.2.1',
     url='https://github.com/uktrade/directory-sso-api-client',
     license='MIT',
     author='Department for International Trade',
@@ -13,34 +13,34 @@ setup(
     long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=[
-        'directory_client_core>=6.0.0,<7.0.0',
+        'directory_client_core>=6.1.0,<7.0.0',
     ],
     extras_require={
         'test': [
-            'django>=2.2,<3.0a1',
-            'requests>=2.18.4,<3.0.0',
-            'pytest==3.6.0',
-            'pytest-cov==2.3.1',
-            'flake8==3.0.4',
-            'requests_mock==1.1.0',
-            'codecov==2.0.9',
-            'twine>=1.11.0,<2.0.0',
+            'django>=2.2.10,<3.0a1',
+            'requests>=2.22.0,<3.0.0',
+            'pytest==5.3.5',
+            'pytest-cov==2.8.1',
+            'flake8==3.7.9',
+            'requests_mock==1.7.0',
+            'codecov==2.0.15',
+            'twine>=2.0.0,<=3.1.1',
             'wheel>=0.31.0,<1.0.0',
-            'setuptools>=38.6.0,<39.0.0',
+            'setuptools>=38.6.0,<=45.2.0',
             'pytest-django>=3.2.1,<4.0.0',
-            'pytest-sugar>=0.9.1,<1.0.0',
+            'pytest-sugar>=0.9.2,<1.0.0',
         ]
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
