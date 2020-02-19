ce8bcae45db8bc7d105b0b23fe9a0d99bbeeb250
kowalcj0 <kowalczykjanusz@gmail.com>
2020-02-19 11:28:01 +0000

    update all requirements to latest available version

--- setup.py
+++ setup.py
@@ -17,18 +17,18 @@ setup(
     ],
     extras_require={
         'test': [
-            'django>=2.2.10,<3.0a1',
-            'requests>=2.22.0,<3.0.0',
-            'pytest==5.3.5',
-            'pytest-cov==2.8.1',
-            'flake8==3.7.9',
-            'requests_mock==1.7.0',
             'codecov==2.0.15',
-            'twine>=2.0.0,<=3.1.1',
-            'wheel>=0.31.0,<1.0.0',
-            'setuptools>=38.6.0,<=45.2.0',
-            'pytest-django>=3.2.1,<4.0.0',
+            'django>=2.2.10,<3.0a1',
+            'flake8==3.7.9',
+            'pytest-cov==2.8.1',
+            'pytest-django>=3.8.0,<4.0.0',
             'pytest-sugar>=0.9.2,<1.0.0',
+            'pytest==5.3.5',
+            'requests>=2.18.4,<3.0.0',
+            'requests_mock==1.7.0',
+            'setuptools>=45.2.0,<50.0.0',
+            'twine>=3.1.1,<4.0.0',
+            'wheel>=0.34.2,<1.0.0',
         ]
     },
     classifiers=[
