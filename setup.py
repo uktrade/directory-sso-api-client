1aa4f476dcbce5b00089c975e884b949a89b49b4
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-07-06 09:46:08 +0100

    use directory-client-core 7.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='directory_sso_api_client',
-    version='7.2.2',
+    version='7.2.3',
     url='https://github.com/uktrade/directory-sso-api-client',
     license='MIT',
     author='Department for International Trade',
@@ -12,7 +12,7 @@ setup(
     long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=[
-        'directory_client_core>=7.2.4,<=8.0.0',
+        'directory_client_core>=7.2.5,<=8.0.0',
     ],
     extras_require={
         'test': [
