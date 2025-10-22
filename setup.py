a7d1476737a6598955eac26cb387946f65a895fd
davidu1975 <david.urquhart@digital.trade.gov.uk>
2025-10-22 12:11:29 +0100

    New Signup and Verification code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='directory_sso_api_client',
-    version='7.2.8',
+    version='7.3.0',
     url='https://github.com/uktrade/directory-sso-api-client',
     license='MIT',
     author='Department for International Trade',
