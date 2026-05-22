fc52009fa1b75077ab77c360caab053978459ce7
divyaparameswaran <divya.parameswaran@digital.trade.gov.uk>
2026-05-22 15:08:55 +0100

    Call the directory-sso user/by-email endpoint

--- tests/test_user.py
+++ tests/test_user.py
@@ -19,6 +19,10 @@ class UserAPIClientTest(TestCase):
     def test_get_session_user(self, stub):
         self.client.get_session_user(session_id=1)
 
+    @stub_request('https://example.com/api/v1/api/v1/user/by-email/', 'get')
+    def test_get_user_by_email(self, stub):
+        self.client.get_user_by_email(email='test@test1234.com')
+
     @stub_request('https://example.com/oauth2/user-profile/v1/', 'get')
     def test_get_oauth2_user_profile(self, stub):
         self.client.get_oauth2_user_profile('123')
@@ -232,6 +236,7 @@ class UserAPIClientTest(TestCase):
     def test_get_user_lesson_completed(self, stub):
         data = {'service': 'great', 'lesson_page': 'dashboard'}
         self.client.get_user_lesson_completed(sso_session_id=999, **data)
+    
 
     @stub_request('https://example.com/api/v1/vod-activity/latest/?video_id=1', 'get')
     def test_get_latest_user_vod_activity(self, stub):
